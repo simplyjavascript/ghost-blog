a427cfedbe3ce3cd2da373f99c995286d1265e22
Hari <simplyjavascript@gmail.com>
2020-10-19 22:53:26 -0400

    Commit 2 - added ghost theme css globally and tested with index file

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,38 +1,33 @@
 export default {
   // Global page headers (https://go.nuxtjs.dev/config-head)
   head: {
-    title: 'ghost-blog',
+    title: "ghost-blog",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      { hid: "description", name: "description", content: "" }
     ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
-    ]
+    link: [{ rel: "icon", type: "image/x-icon", href: "/favicon.ico" }]
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: [
-  ],
+  css: ["~/assets/styles/main.css"],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: [
-  ],
+  plugins: [],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
 
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
-  buildModules: [
-  ],
+  buildModules: [],
 
   // Modules (https://go.nuxtjs.dev/config-modules)
   modules: [
     // https://go.nuxtjs.dev/bootstrap
-    '@nuxtjs/bulma',
+    "@nuxtjs/bulma",
     // https://go.nuxtjs.dev/axios
-    '@nuxtjs/axios',
+    "@nuxtjs/axios"
   ],
 
   // Axios module configuration (https://go.nuxtjs.dev/config-axios)
@@ -46,6 +41,6 @@ export default {
           customProperties: false
         }
       }
-    },
+    }
   }
-}
+};
--- pages/index.vue
+++ pages/index.vue
@@ -1,22 +1,49 @@
 <template>
-  <section class="hero is-primary">
-  <div class="hero-body">
-    <div class="container">
-      <h1 class="title">
-        Primary title
-      </h1>
-      <h2 class="subtitle">
-        Primary subtitle
-      </h2>
+  <section class="hero is-medium">
+    <div class="hero-head">
+      <div class="container">
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+
+          <div id="navbarBasicExample" class="navbar-menu">
+            <div class="navbar-start">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <a class="navbar-item">
+                Blog Posts
+              </a>
+            </div>
+
+            <div class="navbar-end">
+              <div class="navbar-item">
+                <a href="#">
+                  <i class="fab fa-facebook"></i>
+                </a>
+                <a href="#">
+                  <i class="fab fa-twitter"></i>
+                </a>
+                <a href="#">
+                  <i class="fas fa-rss-square"></i>
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </div>
     </div>
-  </div>
-</section>
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <img src="https://cdn.emk.dev/templates/bulma-logo-light.png" width="150" height="40">
+        <h2 class="subtitle">
+          Ghost's Casper theme made with Bulma
+        </h2>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {};
 </script>
 
-<style>
-
-</style>
+<style></style>
